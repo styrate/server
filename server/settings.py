23205741ed1c4aae6742ffa3954088a8be04f51b
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-03-05 16:05:36 +0000

    minor fix

--- server/settings.py
+++ server/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from GLOBAL_VAR import *
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f^l*lhy_u&t!vmg0l1b4&9p&%6xng0q8!pzpean5&tn@)79slc'
+SECRET_KEY = GV_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
